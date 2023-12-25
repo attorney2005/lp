14826369a20a506c127c649519c22ef12af804bb
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2023-12-26 00:12:24 +0300

    Add form handling

--- js/index.js
+++ js/index.js
@@ -5,9 +5,32 @@ function onEntry(entry) {
     }
   });
 }
-let options = { threshold: [0.5] };
+
+let options = {threshold: [0.5]};
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.element-anim-btn');
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+// данные для отправки
+const leadForm = document.getElementById("lead-form");
+const leadFormSubmitButton = document.getElementById("lead-form-submit-button");
+leadFormSubmitButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  // данные для отправки
+  const leadName = leadForm.leadName.value;
+  const leadPhone = leadForm.leadPhone.value;
+
+  const response = await fetch("http://localhost:3000/lead", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      name: leadName,
+      phone: leadPhone
+    })
+  });
+  if (response.status !== 202) {
+    console.error("Server error")
+  }
+});
